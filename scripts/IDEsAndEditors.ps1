2cd2ef8e4876ac2de84c2d912383f4aad9a4d089
Hugo Pacheco <hugopacheco@users.noreply.github.com>
2022-02-08 10:54:44 +0100

    Visual Studio 2022 Professional

--- scripts/IDEsAndEditors.ps1
+++ scripts/IDEsAndEditors.ps1
@@ -2,13 +2,14 @@ choco install -y vscode
 choco install -y sublimetext3
 choco install -y sublimemerge
 
-choco install -y visualstudio2019enterprise
+choco install -y visualstudio2022professional
 Update-SessionEnvironment #refreshing env due to Git install
-choco install -y visualstudio2019-workload-azure
-choco install -y visualstudio2019-workload-netcoretools
-choco install -y visualstudio2019-workload-netweb
+choco install -y visualstudio2022-workload-azure
+choco install -y visualstudio2022-workload-netweb
 
 choco install -y sql-server-management-studio
 
 choco install -y resharper
 choco install -y jetbrains-rider
+
+choco install -y ncrunch-vs2022
